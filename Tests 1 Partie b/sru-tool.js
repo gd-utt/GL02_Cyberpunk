f9f07ee43fa80b15aade4b50ab443007b9f3aec9
Yves Migue <yves.apeapea_migue@utt.fr>
2025-12-05 15:31:42 +0100

    Modification generale du sru tool pour respecter les specs

--- Tests 1 Partie b/sru-tool.js
+++ Tests 1 Partie b/sru-tool.js
@@ -5,9 +5,11 @@ const { program } = require("commander");
 
 program
     .argument("<file>", "fichier .cru")
-    .option("--cours <type>", "filtrer par type de cours (CM, TD, TP)")
-    .option("--matiere <name>", "filtrer par matière (GL02, GL03...)")
-    .option("--salle <room>", "filtrer par salle (C006, C007...)")
+    .option("--cours <type>", "Affiche la salle associe a un cours (CM, TD, TP)")
+    .option("--matiere <nomMatiere>", "filtrer par matière (GL02, GL03...)")
+    .option("-s ,--salle <salle>", "Affiche les dates pour lesquelles une salle est libre ")
+    .option("-sc ,--salleCreneau <creneau>", "Affiche les salle qui sont libres a un creneau donne (jour/HeureDebut/HeureFin) Format heure xx:xx")
+    .option("-c, --capaciteMax <salle>", "Affiche la capacite maximale d'une salle donnee")
     .parse(process.argv);
 
 const options = program.opts();
@@ -66,6 +68,20 @@ function parseCRU(path) {
     return entries;
 }
 
+/* ------------------ CLASSEUR ------------------ */
+function classeur(table){
+    const mapOrdre = new Map();
+    odreJour = ["L", "MA", "ME", "J", "V"];
+    odreJour.forEach((jour, i) => {
+        mapOrdre.set(jour,i);
+    });
+    table.sort((j1, j2) => {
+        const i1 = mapOrdre.get(j1['jour']);
+        const i2 = mapOrdre.get(j2['jour']);
+        return i1-i2;
+    });
+}
+
 /* ------------------ EXECUTION ------------------ */
 
 const data = parseCRU(cruFile);
@@ -75,24 +91,96 @@ let result = data;
 
 if (options.cours) {
     result = result.filter(e => e.type.toUpperCase() === options.cours.toUpperCase());
+    if (result.length === 0) {
+        console.log("Aucun cours trouvé selon le critère.");
+        process.exit(0);
+    }else{
+        console.log(`Les salles occupees par un ${options.cours.toUpperCase()} sont : `);
+        //classe les salles selon le jour 
+        classeur(result);
+        for (let e of result) {
+            console.log(
+                ` Salle ${e.salle} | ${e.jour} ${e.h1}-${e.h2} | ${e.matiere}`
+            );
+        }
+    }
+    
 }
 
-if (options.matiere) {
+/*if (options.matiere) {
     result = result.filter(e => e.matiere.toUpperCase() === options.matiere.toUpperCase());
+    if (result.length === 0) {
+        console.log("Aucun elements trouvé selon les critères.");
+        process.exit(0);
+    }else{
+        console.log(`Les matieres donnant des ${options.cours.toUpperCase()} sont : `);
+        for (let e of result) {
+            console.log(
+                ` ${e.matiere} | ${e.jour} ${e.h1}-${e.h2} | Salle ${e.salle} | Cap=${e.capacitaire}`
+            );
+        }
+    }
+}*/
+
+if (options.capaciteMax) {
+    result = result.filter(e => e.salle.toUpperCase() === options.capaciteMax.toUpperCase());
+    if (result.length === 0) {
+        console.log("La salle n'a pas ete trouvee ");
+        process.exit(0);
+    }else{
+        const capMax = result.reduce((max, sCap) => {
+            const valSalle = sCap['capacitaire'];
+            return Math.max(max, valSalle);
+        }, -Infinity);
+        console.log(`La capicite max de la salle ${options.capaciteMax.toUpperCase()} est : ${capMax}`);
+    }
 }
 
-if (options.salle) {
-    result = result.filter(e => e.salle.toUpperCase() === options.salle.toUpperCase());
+if (options.salle){
+    result = result.filter(e => (e.matiere.toUpperCase() === "LIBRE") && (e.salle.toUpperCase() == options.salle.toUpperCase()));
+    if (result.length === 0) {
+        console.log("Aucune salle n'est libre pour le moment");
+        process.exit(0);
+    }else{
+        console.log(`La salle ${options.salle.toUpperCase()} est libre aux horaires suivants :  `);
+        for (let e of result) {
+            console.log(
+                ` ${e.jour} ${e.h1}-${e.h2} | Cap=${e.capacitaire}`
+            );
+        }
+    }
 }
 
-if (result.length === 0) {
-    console.log("Aucun cours trouvé selon les critères.");
-    process.exit(0);
+if (options.salleCreneau){
+    result = result.filter(e => (e.matiere.toUpperCase() === "LIBRE"));
+    if(result.length === 0){
+         console.log("Aucune salle n'est libre");
+    }else{
+        const creneau = options.salleCreneau.split("/");
+        const try1 = result.filter(e => (e.h1 === "H="+creneau[1] && e.h2 === creneau[2] && e.jour === creneau[0]));
+        if(try1.length === 0){
+            const try2 = result.filter(e => (e.h1 === "H="+creneau[1]));;
+            if(try2.length === 0){
+                console.log("Le creneau donnee n'a aucune corexpondance");
+            }else{
+                console.log("Le creneau n'a pas correspondance mais des horaires sont utilisables");
+                for (let e of try2) {
+                    console.log(
+                        `${e.salle} | ${e.jour} ${e.h1} jusqu'a ${e.h2} | Cap=${e.capacitaire}`
+                    );
+                }
+            }
+        }else{
+            console.log("Les salles libres sont : ");
+            for (let e of try1) {
+                console.log(
+                    `${e.salle} | ${e.jour} ${e.h1}-${e.h2} | Cap=${e.capacitaire}`
+                );
+            }
+        }
+    }
 }
 
-console.log("Cours trouvés :");
-for (let e of result) {
-    console.log(
-        ` ${e.matiere} | ${e.type} | ${e.jour} ${e.h1}-${e.h2} | Salle ${e.salle} | Cap=${e.capacitaire}`
-    );
-}
+process.exit(0);
+
+
